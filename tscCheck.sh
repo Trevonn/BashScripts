f2495fd674884da8a2dd4402cf130d3730f7a64f
Trevonn <Trevonn@users.noreply.github.com>
2022-12-10 14:42:33 +0000

    Simplify the script to use less code

--- tscCheck.sh
+++ tscCheck.sh
@@ -1,23 +1,10 @@
 #!/bin/bash
 
-# This function checks the processor's current clocksource
-# If that clocksource is TSC it prints yes. If not it prints no
-
-function arewetsc() {
-    tsc=$(cat /sys/devices/system/clocksource/clocksource*/current_clocksource)
-    if [ $tsc == "tsc" ] 
-    then
-        echo "Yes"
-    else
-        echo "No"
-    fi
-}
-
-# Store the result of arewetsc in a variable
-tscCheck=$(arewetsc)
+# Store the current clocksource in a variable
+tscCheck=$(cat /sys/devices/system/clocksource/clocksource*/current_clocksource)
 
 # Check the value of the variable. Print a message to journalctl depending on the result
-if [ $tscCheck == "Yes" ] 
+if [ $tscCheck == "tsc" ] 
 then
     echo 'TSC active' | systemd-cat -p info
 else
