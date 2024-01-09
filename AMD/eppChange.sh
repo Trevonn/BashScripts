23d25c0da228987d4a777616c8606b425c44bff3
Trevonn <Trevonn@users.noreply.github.com>
2024-01-09 01:39:25 +0000

    Update eppChange.sh to use cpupower

--- AMD/eppChange.sh
+++ AMD/eppChange.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-local choice=""
-echo "AMD EPP Changer 1.0"
+eppPreference=/sys/devices/system/cpu/cpu0/cpufreq/energy_performance_preference
+choice=""
+echo "AMD EPP Changer 1.1"
 echo "Available preferences: $(cat /sys/devices/system/cpu/cpu0/cpufreq/energy_performance_available_preferences)"
+echo "Current EPP Preference $(cat $eppPreference)"
 read -p "Choose an option: " choice
-ls /sys/devices/system/cpu/cpu*/cpufreq/energy_performance_preference | { 
-    while read cpu ; do
-        sudo sh -c "echo "$choice" > "$cpu""
-    done
-}
-echo "printing current epp values"
-cat /sys/devices/system/cpu/cpu*/cpufreq/energy_performance_preference
+sudo cpupower set --epp $choice
+echo "Current EPP Preference $(cat $eppPreference)"
