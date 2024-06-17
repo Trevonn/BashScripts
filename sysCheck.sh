9315f08fcbbb3260e8902255e533f2d751255b2d
Trevonn <Trevonn@users.noreply.github.com>
2024-06-17 21:54:30 +0100

    Update sysCheck.sh
    Move LLVM version into GPU section
    Report glibc version
    Report Feral GameMode version
    Check if MangoHud is installed
    Check if Feral GameMode is installed

--- sysCheck.sh
+++ sysCheck.sh
@@ -3,7 +3,8 @@
 function sysCheck() {
     local cpufreq=/sys/devices/system/cpu/cpu0/cpufreq/
     local gpuLevel=/sys/class/drm/card1/device/power_dpm_force_performance_level
-
+    
+    echo
     echo "CPU"
     echo "......................"
     echo "Scaling Governor     : $(cat $cpufreq/scaling_governor)"
@@ -15,6 +16,7 @@ function sysCheck() {
     echo "GPU"
     echo "......................"
     echo "Mesa                 : $(vulkaninfo | grep driverVersion | awk '{ print $3 }')"
+    echo "LLVM                 : $(llvm-config --version)"
     echo "OpenGL               : $(glxinfo | grep 'OpenGL core profile version string' | awk '{ print $6 }')"
     echo "Vulkan               : $(vulkaninfo | grep apiVersion | awk '{ print $3 }')"
     echo "AMD Perf State       : $(cat $gpuLevel)"
@@ -22,13 +24,14 @@ function sysCheck() {
     echo "System"
     echo "......................"
     echo "Kernel               : $(uname -r)"
-    echo "LLVM                 : $(llvm-config --version)"
+    echo "glibc                : $(ldd --version | grep ldd | awk '{print $4}')"
     echo "vm.max_map_count     : $(cat /proc/sys/vm/max_map_count)"
     echo "DefaultLimitNOFILE   : $(ulimit -Hn)"
     echo
     echo "Utilities"
     echo "......................"
-    echo "MangoHud             : $(mangohud --version)"
+    echo "MangoHud             : $(if [[ -f /usr/bin/mangohud ]] ; then mangohud --version | cut -c 2- ; else echo 'MangoHud not installed'; fi)"
+    echo "Feral GameMode       : $(if [[ -f /usr/bin/gamemoded ]] ; then gamemoded -v | awk '{ print $3 }' | cut -c 2- ; else echo 'Feral GameMode not installed' ; fi )"
 }
 
 sysCheck > sysCheck.txt
