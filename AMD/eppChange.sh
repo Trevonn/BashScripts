4594d4caf6a419f982ed224ce634c152bdae6a19
Trevonn <Trevonn@users.noreply.github.com>
2024-10-31 12:36:34 +0000

    Remove eppChange.sh and AMD folder

--- AMD/eppChange.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-eppPreference=/sys/devices/system/cpu/cpu0/cpufreq/energy_performance_preference
-choice=""
-echo "AMD EPP Changer 1.1"
-echo "Available preferences: $(cat /sys/devices/system/cpu/cpu0/cpufreq/energy_performance_available_preferences)"
-echo "Current EPP Preference $(cat $eppPreference)"
-read -p "Choose an option: " choice
-sudo cpupower set --epp $choice
-echo "Current EPP Preference $(cat $eppPreference)"
