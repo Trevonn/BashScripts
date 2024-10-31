a2d7b0ff33481fee513fe558c7a3714c6e4f8037
Trevonn <Trevonn@users.noreply.github.com>
2024-10-31 12:09:07 +0000

    Update bash_functions.sh

--- bash_functions.sh
+++ bash_functions.sh
@@ -1,20 +1,17 @@
+#!/bin/bash
 # General
 
-function bakup() {
-    mv $1 $1.bak
+mkcd() {
+    mkdir -p -- "$1" && cd -P -- "$1"
 }
 
-function rstore() {
-    mv $1.bak $1
-}
-
-function nvmeHealth() {
+nvmeHealth() {
     local option="" 
     read -p "Disk: " option
     sudo nvme smart-log -H /dev/nvme$option
 }
 
-function resetDevice() {
+resetDevice() {
     local option=""
     local module=""
     echo "Device Reset 1.0"
@@ -43,123 +40,41 @@ function resetDevice() {
     sudo rmmod $module && sudo modprobe $module
 }
 
-function sdLoop() {
-    local sdService=$1
-    local sdCmd=""
-    echo "Systemd Loop V1.1"
-    echo
-    echo "$sdService chosen"
-    systemctl status $1
-    echo
-    echo " 1: Status"
-    echo " 2: Restart"
-    echo " 3: Enable"
-    echo " 4: Disable"
-    echo " 5: Stop"
-    echo " 6: Start"
-    echo " 7: Mask"
-    echo " 8: Unmask"
-    echo " 9: dameon-reload"
-    echo "10: Exit"
-    echo
-    read -p "Choose an option: " sdCmd
-    case $sdCmd in
-    1)
-        systemctl status $1
-        sdLoop $1 ;;
-    2)
-        systemctl restart $1
-        sdLoop $1 ;;
-    3)
-        systemctl enable $1
-        sdLoop $1 ;;
-    4)
-        systemctl disable $1
-        sdLoop $1 ;;
-    5)
-        systemctl stop $1
-        sdLoop $1 ;;
-    6)
-        systemctl daemon-reload
-        systemctl start $1
-        sdLoop $1 ;;
-    7)
-        systemctl mask $1
-        sdLoop $1 ;;
-    8)
-        systemctl unmask $1
-        sdLoop $1 ;;
-    9)
-        systemctl daemon-reload
-        sdLoop $1 ;;
-    10)
-        echo "Exiting" ;;
-    *)
-        return
-    esac
-}
-
-function mkcd() {
-    mkdir -p -- "$1" && cd -P -- "$1"
-}
-
-function findFiles() {
+findFiles() {
     find -type f -name "*.$1"
 }
 
-function findFiles2() {
+findFiles2() {
     find ./ -type f \( -iname \*."$1" -o -iname \*."$2" \)
 }
 
-function findFolders() {
+findFolders() {
     find -maxdepth 1 -type d -name "$1*"
 }
 
-function to7z() {
+to7z() {
     7z a -mx9 "${1%.$2}.7z" "$1"
 }
 
-function toZstFolder() {
-    findFolders $1 | {
-        while read file ; do
-            tar -I "zstd --ultra -22" -cf $file.tar.zst $file
-        done
-    }
+toZst() {
+    tar -I "zstd --ultra -22 -T$(nproc)" -cf $1.tar.zst $1
 }
 
-function to7zFolder() {
-    findFolders $1 | {
-        while read file ; do
-            7z a -mx9 "$file.7z" "$file" -p $2
-        done
-    }
-}
-
-function delete() {
+delete() {
     if [[ -d "$1" ]]
     then
-        sudo rm -rf "$1"
+        sudo rm -r "$1"
         echo "Deleting folder $1"
     elif [[ -f "$1" ]]
     then
-        sudo rm -f "$1"
+        sudo rm "$1"
         echo "Deleting file $1"
     else
         echo "$1 not found"
     fi
 }
 
-function folderEmptyDelete() {
-    if [[ -z "$(ls -A $1)" ]]
-    then
-        echo "$1 is empty"
-    else
-        sudo rm -rf $1/*
-        echo "Deleted contents of $1"
-    fi
-}
-
-function tscCheck() {
+tscCheck() {
     if [[ $(cat /sys/devices/system/clocksource/clocksource0/current_clocksource) == "tsc" ]]
     then
         echo "TSC is active" | systemd-cat -p info
@@ -171,13 +86,28 @@ function tscCheck() {
     return
 }
 
-function storageCleanup() {
-    yay -Scc
-    delete /var/log/journal/*
-    delete /var/lib/systemd/coredump/*
+clearCacheFiles() {
+    find $XDG_CACHE_HOME -type d -name "$1" | while read folder
+    do
+      find $folder -name "*" -type f -mtime +1 -delete
+    done
 }
 
-function debugLogs() {
+storageCleanup() {
+    yay -Scc
+    sudo rm -r /var/log/journal/*
+    echo "Cleared /var/log/journal"
+    sudo rm -rf /var/lib/systemd/coredump/*
+    echo "Cleared /var/lib/systemd/coredump"
+    find "$XDG_CACHE_HOME" -name "ksycoca6*" -type f -mtime +1 -delete
+    find "$XDG_CACHE_HOME/qtshadercache-x86_64-little_endian-lp64/" -name "*" -type f -mtime +1 -delete
+    find "$XDG_CACHE_HOME" -name "*.qmlc" -type f -mtime +1 -delete
+    find "$XDG_CACHE_HOME" -name "*.jsc" -type f -mtime +1 -delete
+    find "$XDG_CACHE_HOME" -name "qqpc_opengl" -type f -mtime +1 -delete
+    echo "Cleared various cache files"
+}
+
+debugLogs() {
     mkdir debugLogs
     cd debugLogs
     dmesg > dmesg.log
@@ -194,15 +124,15 @@ function debugLogs() {
 
 # Media
 
-function toFlac() {
+toFlac() {
     findFiles $1 | parallel ffmpeg -i "{}" -c:a flac -sample_fmt s32 "{.}.flac"
 }
 
-function flacToOpus() {
+flacToOpus() {
     findFiles flac | parallel opusenc --bitrate $1 "{}" "{.}.opus"
 }
 
-function tagMusic() {
+tagMusic() {
     if [[ "$1" == "title" ]]
     then
         kid3-cli -c "select *.opus" -c "totag '%{title}' 2"
@@ -214,11 +144,11 @@ function tagMusic() {
     fi
 }
 
-function downloadMusic() {
+downloadMusic() {
     yt-dlp --format 251 --extract-audio --audio-format "opus" $1 -o "%(title)s.%(ext)s"
 }
 
-function mkvDefaultTrack() {
+mkvDefaultTrack() {
     echo "MKV Default Track Changer 1.0"
     echo "This script lets you change the default track properties in an MKV"
     echo ""
@@ -235,41 +165,41 @@ function mkvDefaultTrack() {
     findFiles mkv | parallel mkvpropedit "{}" --edit track:$trackType$trackNum --set flag-default=$trackDef --set flag-forced=$trackForced
 }
 
-function toMKV() {
+toMKV() {
     findFiles $1 | parallel mkvmerge -o "{.}.mkv" "{}"
 }
 
-function removeTracks() {
+removeTracks() {
 # Audio and Subtitle tracks not chosen by $1 and $2 will be removed
     findFiles mkv | parallel mkvmerge -o "Muxed/{}" -a $1 -s $2 "{}"
 }
 
-function addSubs() {
+addSubs() {
     findFiles $1 | parallel mkvmerge -o "Muxed/{}" "{}" "{.}".srt
 }
 
-function toJxl() {
+toJxl() {
     local type=""
     read -p "Image Type: " type
     echo ".$type images chosen"
     findFiles $type | parallel cjxl "{}" "{.}.jxl"
 }
 
-function videoState() {
+videoState() {
     # $1 The file containing the list of videos
     cat $1 | while read video
     do
-#         echo $video
+#       echo $video
         if [[ -f "$video" ]]
         then
-            bakup "$video"
+            mv $video $video.bak
             echo "Disabled $video"
         elif [[ -f "$video.bak" ]]
         then
-            rstore "$video"
+            mv $video.bak $video
             echo "Restored $video"
         else
-            echo "Video not found"
+            echo "Video/s not found"
         fi
     done
 }
@@ -278,54 +208,55 @@ function videoState() {
 
 # Gaming-Wine
 
-function wineKill() {
+wineKill() {
     kill -9 $(ps -ef | grep -E -i "(wine|processid|\.exe)" | awk "{print $2}")
     killall -9 pressure-vessel-adverb
 }
 
-function downloadDirectX() {
+downloadDirectX() {
     curl -s https://api.github.com/repos/HansKristian-Work/vkd3d-proton/releases/latest | grep -o "https.*zst" | wget -i -
     curl -s https://api.github.com/repos/doitsujin/dxvk/releases/latest | grep -om 1 "https.*tar.gz" | wget -i -
 }
 
-function dx11() {
+dx11() {
     echo "Installing DXVK (DirectX 8,9,10,11)"
     $HOME/Sync/Gaming/DXVK/setup_dxvk.sh install --symlink
 }
-function dx12() {
+
+dx12() {
     echo "Installing VKD3D-Proton (DirectX 12)"
     $HOME/Sync/Gaming/VKD3D-Proton/setup_vkd3d_proton.sh install --symlink
 }
 
-function dxSetup() {
+dxSetup() {
     dx11
     dx12
 }
 
 # Gaming-Emulation
 
-function crom() {
+crom() {
     case $1 in
     "chdDVD")
-        findFiles2 iso bin | parallel chdman createdvd -f -i "{}" -o "{.}.chd" ;;
+        findFiles2 iso bin | parallel chdman createdvd -f -i "{}" -o "{.}.chd -c zstd" ;;
     "chdCD")
-        findFiles cue | parallel chdman createcd -f -i "{}" -o "{.}.chd"gr ;;
+        findFiles cue | parallel chdman createcd -f -i "{}" -o "{.}.chd" ;;
     *)
         echo "Incorrect or no option chosen"
     esac
 }
 
-function crom7z {
+crom7z() {
     findFiles $1 | parallel --bar 7z a -mx9 "{.}.7z" "{}"
 
 }
 
-function recrom() {
+reCrom() {
     ark --batch --autodestination *.7z
     mvrom $1
 }
 
-function mvrom() {
+mvrom() {
     local type="$1"
     local dest=""
     case $1 in
@@ -383,22 +314,7 @@ function mvrom() {
 
 # Gaming-Misc
 
-function backupGameSaves() {
-    local savesDir="$HOME/Games/Game Saves"
-    local savesFile="Game Saves - $(date +"%d-%m-%Y")"
-    7z a -mx9 "$savesFile.7z" "$savesDir"
-}
-
-function changeGameIntro() {
-    if [[ -f "videos.txt" ]]
-    then 
-        videoState "videos.txt"
-    else
-        echo "videos.txt not found"
-    fi
-}
-
-function changeGPUState() {
+changeGPUState() {
     local gpuLevel=/sys/class/drm/card1/device/power_dpm_force_performance_level
     echo "Current GPU Level: $(cat $gpuLevel)"
     echo "Setting GPU Level to $1"
@@ -408,68 +324,28 @@ function changeGPUState() {
 
 # Pi
 
-function piBackupSettings() {
-    local option=""
-    echo "Pi Backup Settings V1"
-    echo "1 - Backup"
-    echo "2 - Restore"
-    read -p "Would you like to backup or restore? " option
-    case $option in
-    1)
-        local backupFile="piSettings_$(date +"%d-%m-%Y").tar"
-        local backupDir="/srv/nfs/Backup/Linux/pi/"
-        local tempFolder="piSettings"
-        # Create and move to the folder to extract the settings into
-        mkdir $tempFolder
-        cd $tempFolder
-        # Create the folder layout for extracted settings
-        echo "Creating folders"
-        mkdir {etc,var,var/lib,.local,.local/state,.ssh}
-        # Copy all the settings files
-        echo "Copying settings"
-        sudo cp -r -p /var/lib/jellyfin var/lib/
-        sudo cp -r -p /etc/jellyfin etc/
-        cp -p $HOME/.profile .profile
-        cp -p $HOME/.bashrc .bashrc
-        cp -p $HOME/.ssh/authorized_keys .ssh/authorized_keys
-        cp -r -p /etc/samba etc/
-        cp -p /etc/fstab etc/fstab
-        cp -r -p $HOME/.local/state/syncthing .local/state
-        cp -p /boot/cmdline.txt cmdline.txt
-        cd ../
-        echo "Creating tar file"
-        sudo tar -cf "$backupFile" $tempFolder/
-        echo "Copying $backupFile to $backupDir"
-        cp -p $backupFile $backupDir
-        sudo rm -R $tempFolder
-        rm -f $backupFile
-        echo "Removing leftover files"
-        echo "Done" ;;
-    2)
-        cd $HOME/piSettings
-        # Set pi as the owner of all the files
-        echo "Changing the owner and group of all files to pi"
-        sudo chown -R pi:pi *
-        echo "Restoring files"
-        cp -p .profile $HOME/.profile
-        cp -p .bashrc $HOME/.bashrc
-        cp -p .ssh/authorized_keys $HOME/.ssh/authorized_keys
-        cp -r -p .local/state/syncthing $HOME/.local/state
-        sudo cp -r -p var/lib/jellyfin /var/lib/
-        sudo cp -r -p etc/jellyfin /etc/
-        sudo cp -r -p etc/samba /etc/
-        sudo cp -p etc/fstab /etc/fstab
-        sudo cp -p cmdline.txt /boot/cmdline.txt
-        sudo chown -R jellyfin:adm /etc/jellyfin/
-        sudo chown -R jellyfin:adm /var/lib/jellyfin ;;
-    *)
-        echo "Incorrect or no option chosen"
-    esac
+piBackupSettings() {
+    local backupFile="/srv/nfs/Backup/Linux/Raspberry Pi/Raspberry Pi Settings - $(date +"%Y-%m-%d").tar.zst"
+    local targets="/home/pi/.profile /home/pi/.bashrc /home/pi/.ssh/authorized_keys /etc/samba /etc/fstab /home/pi/.local/state/syncthing"
+    sudo tar -P -I "zstd --ultra -22 -T$(nproc)" -cf "$backupFile" $targets
+}
+
+jellyfinBackup() {
+    local backupFile="/srv/nfs/Backup/Linux/Raspberry Pi/Jellyfin Backup - $(date +"%Y-%m-%d").tar"
+    local targets="/var/lib/jellyfin /etc/jellyfin"
+    sudo tar -P -cf "$backupFile" $targets
+}
+
+restoreTar() {
+    local backupFile=""
+    ls -l *.{tar,tar.zst}
+    read -p "Which tar file would you like to restore: " backupFile
+    sudo tar -P -xf "$backupFile"
 }
 
 # Git
 
-function buildMesa() {
+buildMesa() {
     local oldLocation=$PWD
     cd "$HOME/Git/mesa"
     git pull
@@ -480,7 +356,7 @@ function buildMesa() {
     rm -rf $HOME/.cache/mesa_shader_cache_sf
 }
 
-function configureMesa() {
+configureMesa() {
     local oldLocation=$PWD
     local option=""
     local mesaSync="$HOME/Sync/Gaming/Mesa"
@@ -512,7 +388,7 @@ function configureMesa() {
 
 # Misc
 
-function buildLinuxTSC() {
+buildLinuxTSC() {
     local patches="$HOME/Sync/Config/Kernel/"
     local kernel=""
     echo "1: linux"
@@ -534,14 +410,14 @@ function buildLinuxTSC() {
     time makepkg --syncdeps --skipinteg
 }
 
-function printAfter() {
+printAfter() {
     string=$1
     searchstring="$2"
     result=${string#*$searchstring}
     echo $result
 }
 
-function isInstalled() {
+isInstalled() {
     if [[ -f $1 ]]
     then
         echo "Installed"
@@ -550,18 +426,38 @@ function isInstalled() {
     fi
 }
 
-function cpuCheck() {
+sysCheck() {
+    echo "System Check V2.01"
+    echo
+    echo "System"
+    echo "......................"
+    echo "Kernel               : $(uname -r)"
+    echo "glibc                : $(ldd --version | grep ldd | awk '{print $4}')"
+    echo "vm.max_map_count     : $(cat /proc/sys/vm/max_map_count)"
+    echo "DefaultLimitNOFILE   : $(ulimit -Hn)"
+    echo "Steam udev rules     : $(isInstalled /usr/lib/udev/rules.d/70-steam-input.rules)"
+    echo
+    cpuCheck
+    echo
+    gpuCheck
+    echo
+    utilitiesCheck
+}
+
+cpuCheck() {
     local cpufreq=/sys/devices/system/cpu/cpu0/cpufreq/
+    local turboBoost="$(if [[ $(cat $cpufreq/boost) == 1 ]]; then echo "On"; else echo "Off"; fi)"
     echo "CPU"
     echo "......................"
     echo "Scaling Governor     : $(cat $cpufreq/scaling_governor)"
     echo "Scaling Driver       : $(cat $cpufreq/scaling_driver)"
+    echo "Turbo Boost          : $turboBoost"
     echo "AMD P-State Mode     : $(cat /sys/devices/system/cpu/amd_pstate/status)"
     echo "EPP Preference       : $(if [ -f $cpufreq/energy_performance_preference ]; then cat $cpufreq/energy_performance_preference; else echo "N/A"; fi)"
     echo "Clocksource          : $(cat /sys/devices/system/clocksource/clocksource0/current_clocksource)"
 }
 
-function gpuCheck() {
+gpuCheck() {
     local gpuLevel=/sys/class/drm/card1/device/power_dpm_force_performance_level
     echo "GPU"
     echo "......................"
@@ -574,35 +470,17 @@ function gpuCheck() {
     echo "AMDGPU Perf Level    : $(cat $gpuLevel)"
 }
 
-function utilitiesCheck() {
+utilitiesCheck() {
     echo "Utilities"
     echo "......................"
     echo "MangoHud             : $(if [[ -f /usr/bin/mangohud ]]; then mangohud --version | cut -c 2-; else echo "Not installed"; fi)"
     echo "Feral GameMode       : $(if [[ -f /usr/bin/gamemoded ]]; then gamemoded -v | awk '{ print $3 }' | cut -c 2-; else echo "Not installed"; fi )"
     echo "Gamescope            : $(isInstalled /usr/bin/gamescope)"
-    echo "Steam udev rules     : $(isInstalled /usr/lib/udev/rules.d/70-steam-input.rules)"
 }
 
-function sysCheck() {
-    echo "System Check V2.00"
-    cpuCheck
-    echo
-    gpuCheck
-    echo
-    echo "System"
-    echo "......................"
-    echo "Kernel               : $(uname -r)"
-    echo "glibc                : $(ldd --version | grep ldd | awk '{print $4}')"
-    echo "vm.max_map_count     : $(cat /proc/sys/vm/max_map_count)"
-    echo "DefaultLimitNOFILE   : $(ulimit -Hn)"
-    echo
-    utilitiesCheck
-}
 
-function test_amd_s2idle() {
-    local dest="amd_s2idle"
-    mkdir $dest
-    cd $dest
+test_amd_s2idle() {
+    mkcd amd_s2idle
     wget https://gitlab.freedesktop.org/drm/amd/-/raw/master/scripts/amd_s2idle.py
     sudo python amd_s2idle.py
 }
