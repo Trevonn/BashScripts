1907ed3751d2f394c89f7b8e14e08b6304aa94f1
Trevonn <Trevonn@users.noreply.github.com>
2024-10-31 12:06:27 +0000

    sysCheck V2.01
    Remove redundant function word before each function declaration
    Add turbo boost and move Steam udev rules to the System section

--- sysCheck.sh
+++ sysCheck.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-function printAfter() {
+printAfter() {
     string=$1
     searchstring="$2"
     result=${string#*$searchstring}
     echo $result
 }
 
-function isInstalled() {
+isInstalled() {
     if [[ -f $1 ]]
     then
         echo "Installed"
@@ -16,18 +16,38 @@ function isInstalled() {
     fi
 }
 
-function cpuCheck() {
+sysCheck() {
+    echo "System Check V2.01"
+    echo
+    echo "System"
+    echo "......................"
+    echo "Kernel               : $(uname -r)"
+    echo "glibc                : $(ldd --version | grep ldd | awk '{print $4}')"
+    echo "vm.max_map_count     : $(cat /proc/sys/vm/max_map_count)"
+    echo "DefaultLimitNOFILE   : $(ulimit -Hn)"
+    echo "Steam udev rules     : $(isInstalled /usr/lib/udev/rules.d/70-steam-input.rules)"
+    echo
+    cpuCheck
+    echo
+    gpuCheck
+    echo
+    utilitiesCheck
+}
+
+cpuCheck() {
     local cpufreq=/sys/devices/system/cpu/cpu0/cpufreq/
+    local turboBoost="$(if [[ $(cat $cpufreq/boost) == 1 ]]; then echo "On"; else echo "Off"; fi)"
     echo "CPU"
     echo "......................"
     echo "Scaling Governor     : $(cat $cpufreq/scaling_governor)"
     echo "Scaling Driver       : $(cat $cpufreq/scaling_driver)"
+    echo "Turbo Boost          : $turboBoost"
     echo "AMD P-State Mode     : $(cat /sys/devices/system/cpu/amd_pstate/status)"
     echo "EPP Preference       : $(if [ -f $cpufreq/energy_performance_preference ]; then cat $cpufreq/energy_performance_preference; else echo "N/A"; fi)"
     echo "Clocksource          : $(cat /sys/devices/system/clocksource/clocksource0/current_clocksource)"
 }
 
-function gpuCheck() {
+gpuCheck() {
     local gpuLevel=/sys/class/drm/card1/device/power_dpm_force_performance_level
     echo "GPU"
     echo "......................"
@@ -40,30 +60,12 @@ function gpuCheck() {
     echo "AMDGPU Perf Level    : $(cat $gpuLevel)"
 }
 
-function utilitiesCheck() {
+utilitiesCheck() {
     echo "Utilities"
     echo "......................"
     echo "MangoHud             : $(if [[ -f /usr/bin/mangohud ]]; then mangohud --version | cut -c 2-; else echo "Not installed"; fi)"
     echo "Feral GameMode       : $(if [[ -f /usr/bin/gamemoded ]]; then gamemoded -v | awk '{ print $3 }' | cut -c 2-; else echo "Not installed"; fi )"
     echo "Gamescope            : $(isInstalled /usr/bin/gamescope)"
-    echo "Steam udev rules     : $(isInstalled /usr/lib/udev/rules.d/70-steam-input.rules)"
-}
-
-function sysCheck() {
-    echo "System Check V2.00"
-    echo
-    echo "System"
-    echo "......................"
-    echo "Kernel               : $(uname -r)"
-    echo "glibc                : $(ldd --version | grep ldd | awk '{print $4}')"
-    echo "vm.max_map_count     : $(cat /proc/sys/vm/max_map_count)"
-    echo "DefaultLimitNOFILE   : $(ulimit -Hn)"
-    echo
-    cpuCheck
-    echo
-    gpuCheck
-    echo
-    utilitiesCheck
 }
 
 sysCheck > sysCheck.txt
