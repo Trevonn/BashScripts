bb51e5199709ed4c105ca253bbee0cf0aa1508e7
Trevonn <Trevonn@users.noreply.github.com>
2024-11-02 16:19:00 +0000

    sysCheck V2.1

--- sysCheck.sh
+++ sysCheck.sh
@@ -1,71 +1,63 @@
 #!/bin/bash
 
-printAfter() {
-    string=$1
-    searchstring="$2"
-    result=${string#*$searchstring}
-    echo $result
-}
-
-isInstalled() {
-    if [[ -f $1 ]]
-    then
-        echo "Installed"
-    else
-        echo "Not installed";
-    fi
-}
-
 sysCheck() {
-    echo "System Check V2.01"
+    local kernelVersion=$(uname -r)
+    local glibcVersion=$(ldd --version | grep ldd | awk '{print $4}')
+    local maxMapCount=$(cat /proc/sys/vm/max_map_count)
+    local limitNOFILE=$(ulimit -Hn)
+    local steamUdevRules=$(isInstalled /usr/lib/udev/rules.d/70-steam-input.rules)
+    local cpufreq=/sys/devices/system/cpu/cpu0/cpufreq/
+    local scaleGov=$(cat $cpufreq/scaling_governor)
+    local scaleDriver=$(cat $cpufreq/scaling_driver)
+    local turboBoost="$(if [[ $(cat $cpufreq/boost) == 1 ]]; then echo "On"; else echo "Off"; fi)"
+    local amdPstateMode=$(cat /sys/devices/system/cpu/amd_pstate/status)
+    local eppPref="$(if [ -f $cpufreq/energy_performance_preference ]; then cat $cpufreq/energy_performance_preference; else echo "N/A"; fi)"
+    local clocksource="$(cat /sys/devices/system/clocksource/clocksource0/current_clocksource)"
+    local gpuLevel=$(cat /sys/class/drm/card1/device/power_dpm_force_performance_level)
+    local gpuName=$(printAfter "$(DRI_PRIME=1! vulkaninfo --summary | grep -m 1 "deviceName")" "= ")
+    local gpuVram=$(DRI_PRIME=1! glxinfo -B | grep "Dedicated video memory:" | awk 'NR==1 { print $4 }')
+    local mesaVersion=$(vulkaninfo | grep driverVersion | awk 'NR==1 { print $3 }')
+    local llvmVersion=$(llvm-config --version)
+    local openglVersion=$(glxinfo | grep "OpenGL core profile version string" | awk '{ print $6 }')
+    local vulkanVersion=$(vulkaninfo | grep apiVersion | awk 'NR==1 { print $3 }')
+    local mangohudCheck=$(if [[ -f /usr/bin/mangohud ]]; then mangohud --version | cut -c 2-; else echo "Not installed"; fi)
+    local gamemodeCheck=$(if [[ -f /usr/bin/gamemoded ]]; then gamemoded -v | awk '{ print $3 }' | cut -c 2-; else echo "Not installed"; fi )
+    local gamescopeCheck=$(isInstalled /usr/bin/gamescope)
+    
+    echo "System Check V2.1"
     echo
     echo "System"
     echo "......................"
-    echo "Kernel               : $(uname -r)"
-    echo "glibc                : $(ldd --version | grep ldd | awk '{print $4}')"
-    echo "vm.max_map_count     : $(cat /proc/sys/vm/max_map_count)"
-    echo "DefaultLimitNOFILE   : $(ulimit -Hn)"
-    echo "Steam udev rules     : $(isInstalled /usr/lib/udev/rules.d/70-steam-input.rules)"
+    echo "Kernel               : $kernel"
+    echo "glibc                : $glibcVersion"
+    echo "vm.max_map_count     : $maxMapCount"
+    echo "DefaultLimitNOFILE   : $limitNOFILE"
+    echo "Steam udev rules     : $steamUdevRules"
     echo
-    cpuCheck
-    echo
-    gpuCheck
-    echo
-    utilitiesCheck
-}
-
-cpuCheck() {
-    local cpufreq=/sys/devices/system/cpu/cpu0/cpufreq/
-    local turboBoost="$(if [[ $(cat $cpufreq/boost) == 1 ]]; then echo "On"; else echo "Off"; fi)"
     echo "CPU"
     echo "......................"
-    echo "Scaling Governor     : $(cat $cpufreq/scaling_governor)"
-    echo "Scaling Driver       : $(cat $cpufreq/scaling_driver)"
+    echo "Scaling Governor     : $scaleGov"
+    echo "Scaling Driver       : $scaleDriver"
     echo "Turbo Boost          : $turboBoost"
-    echo "AMD P-State Mode     : $(cat /sys/devices/system/cpu/amd_pstate/status)"
-    echo "EPP Preference       : $(if [ -f $cpufreq/energy_performance_preference ]; then cat $cpufreq/energy_performance_preference; else echo "N/A"; fi)"
-    echo "Clocksource          : $(cat /sys/devices/system/clocksource/clocksource0/current_clocksource)"
-}
-
-gpuCheck() {
-    local gpuLevel=/sys/class/drm/card1/device/power_dpm_force_performance_level
+    echo "AMD P-State Mode     : $amdPstateMode"
+    echo "EPP Preference       : $eppPref"
+    echo "Clocksource          : $clocksource"
+    echo
     echo "GPU"
     echo "......................"
-    echo "Name                 : $(printAfter "$(DRI_PRIME=1! vulkaninfo --summary | grep -m 1 "deviceName")" "= ")"
-    echo "VRAM                 : $(DRI_PRIME=1! glxinfo -B | grep "Dedicated video memory:" | awk 'NR==1 { print $4 }') MB"
-    echo "Mesa                 : $(vulkaninfo | grep driverVersion | awk 'NR==1 { print $3 }')"
-    echo "LLVM                 : $(llvm-config --version)"
-    echo "OpenGL               : $(glxinfo | grep "OpenGL core profile version string" | awk '{ print $6 }')"
-    echo "Vulkan               : $(vulkaninfo | grep apiVersion | awk 'NR==1 { print $3 }')"
-    echo "AMDGPU Perf Level    : $(cat $gpuLevel)"
-}
-
-utilitiesCheck() {
+    echo "Name                 : $gpuName"
+    echo "VRAM                 : $gpuVram MB"
+    echo "Mesa                 : $mesaVersion"
+    echo "LLVM                 : $llvmVersion"
+    echo "OpenGL               : $openglVersion"
+    echo "Vulkan               : $vulkanVersion"
+    echo "AMDGPU Perf Level    : $gpuLevel"
+    echo
     echo "Utilities"
     echo "......................"
-    echo "MangoHud             : $(if [[ -f /usr/bin/mangohud ]]; then mangohud --version | cut -c 2-; else echo "Not installed"; fi)"
-    echo "Feral GameMode       : $(if [[ -f /usr/bin/gamemoded ]]; then gamemoded -v | awk '{ print $3 }' | cut -c 2-; else echo "Not installed"; fi )"
-    echo "Gamescope            : $(isInstalled /usr/bin/gamescope)"
+    echo "MangoHud             : $mangohudCheck"
+    echo "Feral GameMode       : $gamemodeCheck"
+    echo "Gamescope            : $gamescopeCheck"
 }
 
 sysCheck > sysCheck.txt
